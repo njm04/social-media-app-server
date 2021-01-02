04981c35ccd1ef1f20ffc27ec691fc343741c63b
Neil John Medalla <medallaneiljohn@gmail.com>
2021-01-02 07:48:53 -0700

    implement update prof pic and get users

--- routes/users.ts
+++ routes/users.ts
@@ -3,6 +3,7 @@ import _ from "lodash";
 import bcrypt from "bcryptjs";
 import User, { IUser, validate } from "../models/user";
 import auth from "../middlewares/auth";
+import validateObjectId from "../middlewares/validateObjectId";
 
 const router: Router = express.Router();
 
@@ -48,10 +49,30 @@ router.post("/register", async (req: Request, res: Response) => {
   }
 });
 
+router.patch(
+  "/update-profile-picture/:id",
+  [auth, validateObjectId],
+  async (req: Request, res: Response) => {
+    const options = { new: true };
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { profilePicture: { name: req.body.name, url: req.body.url } },
+      options
+    );
+    if (!user) return res.status(400).send("Invalid user");
+    res.send(user);
+  }
+);
+
 router.get("/search/:name", auth, async (req: Request, res: Response) => {
   const user: IUser[] = await User.find({
     fullName: { $regex: req.params.name, $options: "i" },
   });
+  res.send(user);
+});
+
+router.get("/", auth, async (req: Request, res: Response) => {
+  const user = await User.find({}).select("-password -__v");
   res.send(user);
 });
 
