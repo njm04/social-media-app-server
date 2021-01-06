79fb62a56e3a4a3bb7368828192a2cba7999f74c
Neil John Medalla <medallaneiljohn@gmail.com>
2021-01-06 04:56:54 -0700

    implemented image upload

--- routes/images.ts
+++ routes/images.ts
@@ -1,34 +1,57 @@
 import { Router, Request, Response } from "express";
-import Image from "../models/image";
+import Image, { validate } from "../models/image";
 import auth from "../middlewares/auth";
+import validateObjectId from "../middlewares/validateObjectId";
+import User from "../models/user";
 
 const router: Router = Router();
 
-/*
-  upload image in base64 format, thereby,
-  directly storing it in mongodb
-*/
-router.post("/uploadbase", auth, async (req: Request, res: Response) => {
+router.post("/", [auth], async (req: Request, res: Response) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findById(req.body.userId);
+  if (!user) return res.status(400).send("Invalid user");
+
   try {
-    const newImage = new Image({
-      name: req.body.name,
-      data: req.body.data,
+    const image = new Image({
+      imageData: req.body.imageData,
+      userId: user._id,
     });
-    await newImage.save();
-    res.send(newImage);
+
+    await image.save();
+    res.send(image);
   } catch (error) {
     console.log(error);
     res.status(500).send("Unexpected error occured");
   }
 });
 
-router.post("/", auth, async (req: Request, res: Response) => {
-  const images = await Image.findById(req.body.postImagesId).select(
-    "-__v -imageData._id"
-  );
+// prepared for future use.
+router.patch(
+  "/:id",
+  [auth, validateObjectId],
+  async (req: Request, res: Response) => {
+    const options = { new: true };
+    const image = await Image.findByIdAndUpdate(
+      req.params.id,
+      { imageData: req.body.imageData },
+      options
+    );
 
-  res.send(images);
-});
+    if (!image) return res.status(400).send("Image not found");
+
+    res.send(image);
+  }
+);
+
+// router.post("/", auth, async (req: Request, res: Response) => {
+//   const images = await Image.findById(req.body.postImagesId).select(
+//     "-__v -imageData._id"
+//   );
+
+//   res.send(images);
+// });
 
 router.get("/", auth, async (req: Request, res: Response) => {
   const images = await Image.find({}).select("-__v -imageData._id");
