565511831bba93df1d14a25f9e86bc019600d4b5
Neil John Medalla <medallaneiljohn@gmail.com>
2021-02-17 23:19:35 -0700

    implemented logout endpoint

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,6 +2,7 @@ import express, { Router, Request, Response } from "express";
 import _ from "lodash";
 import bcrypt from "bcryptjs";
 import User, { IUser } from "../models/user";
+import auth from "../middlewares/auth";
 
 const router: Router = express.Router();
 
@@ -17,6 +18,17 @@ router.post("/", async (req: Request, res: Response) => {
 
   const token = user.generateAuthToken();
   res.send(token);
+
+  await User.findByIdAndUpdate(user._id, { status: "active" });
+});
+
+router.patch("/logout/:userId", async (req: Request, res: Response) => {
+  const user: IUser | null = await User.findByIdAndUpdate(req.params.userId, {
+    status: "offline",
+  });
+  if (!user) return res.status(400).send("Invalid user");
+
+  res.send({});
 });
 
 export = router;
