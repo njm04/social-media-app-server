df71a6b410cc1c17ce8d6698a460bba2d248072b
Neil John Medalla <medallaneiljohn@gmail.com>
2021-02-20 22:12:56 -0700

    created statics that consists comment model queries

--- models/comment.ts
+++ models/comment.ts
@@ -1,17 +1,19 @@
-import mongoose, { Document } from "mongoose";
+import mongoose, { Document, Model } from "mongoose";
+import { pick } from "lodash";
 import Joi, { ValidationResult } from "joi";
+import { IComment, IReceivedComment } from "../interfaces/comments";
+import { IUser } from "../interfaces/user";
 
-interface ICreatedBy {
-  _id: string;
-  firstName: string;
-  lastName: string;
-  fullName: string;
-}
-
-export interface IComment extends Document {
-  post: string;
-  comment: string;
-  createdBy: ICreatedBy;
+interface ICommentModel extends Model<IComment> {
+  createComment: (data: IReceivedComment, user: IUser) => IComment;
+  findCommentsByPostId: (
+    postId: mongoose.Types.ObjectId
+  ) => Promise<IComment[]>;
+  findOneCommentAndDelete: (id: mongoose.Types.ObjectId) => Promise<IComment>;
+  findCommentByIdAndUpdate: (
+    id: mongoose.Types.ObjectId,
+    updateComment: string
+  ) => Promise<IComment>;
 }
 
 const Schema = mongoose.Schema;
@@ -34,6 +36,44 @@ const commentSchema = new Schema(
   }
 );
 
+commentSchema.statics.createComment = function (
+  data: IReceivedComment,
+  user: IUser
+): IComment {
+  const comment = new CommentModel(pick(data, ["post", "comment", "userId"]));
+  comment.createdBy = {
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    fullName: user.fullName,
+  };
+
+  return comment;
+};
+
+commentSchema.statics.findCommentsByPostId = async function (
+  postId: mongoose.Types.ObjectId
+): Promise<IComment[]> {
+  return await this.find({
+    post: postId,
+  }).select("-__v -updatedAt");
+};
+
+commentSchema.statics.findOneCommentAndDelete = async function (
+  id: mongoose.Types.ObjectId
+): Promise<IComment> {
+  return await this.findOneAndDelete({ _id: id });
+};
+
+commentSchema.statics.findCommentByIdAndUpdate = async function (
+  id: mongoose.Types.ObjectId,
+  updateComment: string
+): Promise<IComment> {
+  const options = { new: true };
+
+  return await this.findByIdAndUpdate(id, { comment: updateComment }, options);
+};
+
 export const validate = (comment: object): ValidationResult => {
   const schema: Joi.ObjectSchema<IComment> = Joi.object({
     post: Joi.string().required(),
@@ -44,4 +84,8 @@ export const validate = (comment: object): ValidationResult => {
   return schema.validate(comment);
 };
 
-export default mongoose.model<IComment>("Comment", commentSchema);
+const CommentModel: ICommentModel = mongoose.model<IComment, ICommentModel>(
+  "Comment",
+  commentSchema
+);
+export default CommentModel;
